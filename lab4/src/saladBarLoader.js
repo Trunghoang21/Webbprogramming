489d83780d1bb88748fbf6a11fc50cfd484d8480
Trunghoang21 <trunghoang2179@gmail.com>
2024-10-01 11:28:00 +0200

    fetch foundations

--- lab4/src/saladBarLoader.js
+++ lab4/src/saladBarLoader.js
@@ -1,5 +1,39 @@
 export async  function inventoryLoader(){
+    // fix late
     const inventory = { Sallad: { price: 10, foundation: true, vegan: true } };
-    await new Promise(resolve => setTimeout(resolve, 500));
-    return inventory;
+    const url = 'http://localhost:8080/foundations';
+    try{
+        const response = await fetch(url);
+        if (!response.ok){
+            throw new Error(`Response status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log(data);
+        await new Promise(resolve => setTimeout(resolve, 500));
+        const fetchedData = await Promise.all(data.map(item => fetIngredient('foundations',item)))
+        // the returned value from Promise.all is an array of objects, so we need to merge them into one object
+        // the reduce method and the spread operator are used to merge the objects into a single one.
+        const result = fetchedData.reduce((acc, item) => {
+            return {...acc, ...item};
+        }, {});
+        console.log(`From inventoryLoader: ${JSON.stringify(result)}`);
+        return result;
+    }catch(error){
+        console.error(error.message);
+    }
+}
+
+async function fetIngredient(type,name){
+    const url = `http://localhost:8080/${type}/${name}`;
+    try{
+        const response = await fetch(url);
+        if (!response.ok){
+            throw new Error(`Response status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log(`From fetIngredient: ${name} value: ${JSON.stringify(data)}`);
+        return {[name]: data};
+    }catch(error){
+        console.error(error.message);
+    }
 }
